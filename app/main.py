1d86a61ab6422d051f0d661dae8f8335336ec745
lukadsant <lucasbdsaint@gmail.com>
2025-05-26 17:16:01 -0300

    Add cors config

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.endpoints import upload,ocr,validate,validatemultiple
 
 app = FastAPI(title="Validador inteligente de Documentos")
 
+# Configuração do CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(upload.router)
 app.include_router(ocr.router)
 app.include_router(validate.router)
